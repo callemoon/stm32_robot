7aba5600d408eb6ef2475190aa357aba89d0adee
Carl-Magnus Moon <calle.moon@gmail.com>
2016-02-13 21:38:38 +0100

    added comment

--- Robot/Projects/STM32F3-Discovery/Examples/TIM/TIM_PWMOutput/Src/hcsr04.h
+++ Robot/Projects/STM32F3-Discovery/Examples/TIM/TIM_PWMOutput/Src/hcsr04.h
@@ -1,7 +1,7 @@
 #ifndef HCSR04
 #define HCSR04
 
-#include "stm32f3xx_hal.h"
+#include "stm32f3xx_hal.h"  // uint_32
 
 void hcsr04_init(uint32_t instance);
 uint32_t hcsr04_getDistance(uint32_t instance);
