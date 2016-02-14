41a1059b6983b473494764cdaf993f54707d55a6
Carl-Magnus Moon <calle.moon@gmail.com>
2016-02-14 23:09:50 +0100

    clean up

--- Robot/Projects/STM32F3-Discovery/Examples/TIM/TIM_PWMOutput/Src/stm32f3xx_hal_msp.c
+++ Robot/Projects/STM32F3-Discovery/Examples/TIM/TIM_PWMOutput/Src/stm32f3xx_hal_msp.c
@@ -195,24 +195,24 @@ void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *htim)
   
   /* Enable the TIMx global Interrupt */
   HAL_NVIC_EnableIRQ(TIM1_UP_TIM16_IRQn);  
-    
-  /* PB.1 ADC3_IN1 is analog line sensor input */
-  GPIO_InitStruct.Pin = GPIO_PIN_1;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-  GPIO_InitStruct.Alternate = 0;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);  
- 
-  // PC.5 ADC2_IN11 is analog line sensor input */
+
+  // PC.5 ADC2_IN11 is analog line sensor input 1 */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
   GPIO_InitStruct.Alternate = 0;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);  
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* PB.1 ADC3_IN1 is analog line sensor input 2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+  GPIO_InitStruct.Alternate = 0;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
  
-  /* PE.7 ADC3_IN13 is analog line sensor input */
+  /* PE.7 ADC3_IN13 is analog line sensor input 3*/
   GPIO_InitStruct.Pin = GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -220,7 +220,7 @@ void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *htim)
   GPIO_InitStruct.Alternate = 0;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);  
 
-  /* PD.11 ADC3_8 is analog line sensor input */
+  /* PD.11 ADC3_8 is analog line sensor input 4 */
   GPIO_InitStruct.Pin = GPIO_PIN_11;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
